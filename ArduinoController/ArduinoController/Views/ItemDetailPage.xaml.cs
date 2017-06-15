1f5d89b6e3de3e2dd05a16f565c67b5411fe8935
Tomasz Krupa <tomaszkrupa95@gmail.com>
2017-06-15 15:50:42 +0200

    init

---
+++ ArduinoController/ArduinoController/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using ArduinoController.ViewModels;
+
+using Xamarin.Forms;
+
+namespace ArduinoController.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+    }
+}
